a1c0e42fbb5f8de25a6ca5540b32c8ec4c6dfad0
Chun Hao Wang <chunhao137@gmail.com>
2014-08-25 20:17:26 -0700

    completed basic layout of MVP

--- js/graph.js
+++ js/graph.js
@@ -1,30 +1,23 @@
 d3.json("data.json",function(data){
 	var width = 1000;
-	var height =500;
-
-	var widthScale = d3.scale.linear()
-	               .domain([0,width])
-	               .range([0,height])
-
-	var axis = d3.svg.axis()
-	           .scale(widthScale);
+	var height =700;
+	var households = data.population/2.6;
+	
 
 	var canvas = d3.select("body").append("svg")
 	              .attr('width',width)
 	              .attr('height',height)
 	              .append('g')
-	              .attr('transform','translate(30,0)')
+	              .attr('transform','translate(30,-30)')
 	              
-
-
 
 	           canvas.selectAll('rect')
 	              .data(data)
 	              .enter()
 	                 .append('rect')
-	                 .attr('height',function(d){return d.population/100000 })
+	                 .attr('height',function(d){return ((d.population/2.6)*0.584)/14000 })
 	                 .attr('width',30)
-	                 .attr('y',function(d){ return height-d.population/100000 })
+	                 .attr('y',function(d){ return height-((d.population/2.6)*0.584)/14000 })
 	                 .attr("x",function(d,i){return i*100 })
 	                 .attr('fill','blue')
 
@@ -32,13 +25,31 @@ d3.json("data.json",function(data){
 	                 .data(data)
 	                 .enter()
 	                 .append('text')
-	                 .text(function(d){return d.county})
+	                 .text(function(d){return Math.floor((d.population/2.6)*0.584)})
 	                 .attr('x',function(d,i){
 	                 	return i * (width/data.length)
 	                 })
 	                 .attr('y',function(d){
-	                 	return height - (d.population/100000)*1.13
+	                 	return height - (((d.population/2.6)*0.584)/14000)*1.05
 	                 })
+
+	        var x = d3.scale.linear()
+	           .range([0,width/1.09])
+	           .domain([0,data.length-1]);
+
+	        var xAxis = d3.svg.axis()
+	            .scale(x)
+	            .tickFormat(function(d) { return data[d].county })
+	            .orient("bottom");
+
+	         canvas.append('g')
+                .attr('transform','translate(0,' + height + ')')
+	            .call(xAxis)
+
+
+
+	      
+	             
 
 
 })
